f83d5ee94babd25844d54729f0a6b9fb8d6b5d0a
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-05 19:22:35 +0700

    read file and mapto json

--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,87 @@ router.get("/download", function(req, res, next) {
 	res.download(file);
 });
 
-const excelHeaders = [
-	"รหัสนักเรียน",
-	"ชื่อจริง",
-	"นามสกุล",
-	"วันเกิด",
-	"รูปแบบการศึกษา",
-	"ระดับการศึกษา",
-	"ห้อง"
-];
+const mapExcelHeaders = {
+	รหัสนักเรียน: "studentID",
+	ชื่อจริง: "firstName",
+	นามสกุล: "lastName",
+	วันเกิด: "birthDate",
+	รูปแบบการศึกษา: "educationType",
+	ระดับการศึกษา: "educationLevel",
+	ห้อง: "educationRoom"
+};
+const schema = {
+	รหัสนักเรียน: {
+		prop: "studentID",
+		type: String
+	},
+	ชื่อจริง: {
+		prop: "firstName",
+		type: String
+	},
+	นามสกุล: {
+		prop: "lastName",
+		type: String
+	},
+	วันเกิด: {
+		prop: "birthDate",
+		type: Date
+	},
+	รูปแบบการศึกษา: {
+		prop: "educationType",
+		type: String
+	},
+	ระดับการศึกษา: {
+		prop: "educationLevel",
+		type: String
+	},
+	ห้อง: {
+		prop: "educationRoom",
+		type: String
+	}
+};
+function mapArrayToJsonByHeader(array, headers) {
+	return new Promise((resolve, reject) => {
+		let json = {};
+		array.forEach((ele, i) => {
+			const header = headers[i];
+			const value = array[i];
+			json[header] = value;
+		});
+		console.log("json", json);
+		resolve(json);
+	});
+}
+async function mapExcelDataToArrayJson(excelData) {
+	try {
+		let headers = {};
+		/* Preparing headers to map */
+		const excelHeaders = excelData.shift();
+
+		for (let i = 0, { length } = excelHeaders; i < length; i++) {
+			const headerValue = mapExcelHeaders[excelHeaders[i]];
+			if (!headerValue) throw { message: "หัวตาราง excel ผิดรูปแบบ" };
+			headers[i] = headerValue;
+		}
+
+		console.log("excelData", excelData);
+		// console.log("excelData[0]", excelData[0]);
+		// console.log("excelData[1]", excelData[1]);
+		// console.log("excelData[2]", excelData[2]);
+
+		/* Iterate and map each array to json */
+		// for (let i = 0, { length } = excelData; i < length; i++) {
+		// 	console.log("i", i);
+		// 	const row = excelData[i];
+		// 	// console.log("row", row);
+		// 	excelData = await mapArrayToJsonByHeader(row, headers);
+		// }
+
+		return excelData;
+	} catch (error) {
+		throw error;
+	}
+}
 router.post("/upload", async function(req, res) {
 	try {
 		const file = req.files.excel;
@@ -37,20 +109,25 @@ router.post("/upload", async function(req, res) {
 			res.status(400).send("not xlsx but", extension);
 			return;
 		}
-		const rows = await readXlsxFile(savePath, { sheet: 2 });
-		for (let i = 0; i < rows[0].length; i++) {
-			if (rows[0][i] !== excelHeaders[i]) {
-				console.log(rows[0][i], excelHeaders[i]);
-			} else {
-				console.log(excelHeaders[i], "true");
-			}
-		}
-		console.log("rows", rows);
+		// const { rows, errors } = await readXlsxFile(savePath, { schema, sheet: 2 });
+		const { rows, errors } = await readXlsxFile(savePath, { schema, sheet: 2 });
 		fs.unlinkSync(savePath);
 
-		res.status(200).json();
+		console.log("rows::", rows);
+		console.log("errors::", errors);
+
+		if (errors.length !== 0) {
+			res.status(400).send(errors);
+			return;
+		}
+		/* */
+		// console.log(await mapExcelDataToArrayJson(rows));
+		// console.log("rows", rows);
+
+		res.status(200).send(rows);
 	} catch (error) {
 		console.log(error);
+		res.status(400).send(error);
 	}
 });
 module.exports = router;
